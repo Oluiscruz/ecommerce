16a891f18b2682bbba4747e741d8c9d08f6736fa
Luis Alberto <euokira2005@gmail.com>
2024-09-24 18:03:12 -0300

    test

--- src/Components/Header/navbar/sidebar/sidebarItens/itens.jsx
+++ src/Components/Header/navbar/sidebar/sidebarItens/itens.jsx
@@ -2,13 +2,11 @@ import { FaUser } from "react-icons/fa";
 import { IoIosHome } from "react-icons/io";
 import { GoPaperAirplane } from "react-icons/go";
 import "./itens.scss";
-import SearchResponse from "./SearchResponse/SearchRes";
 
 export default function ItensSidebar() {
 
     return (
         <div className="itens">
-            <SearchResponse />
             <a href="#">
                 <IoIosHome /> Início
             </a>
